baf6f5991d8b37b2b40190592877a5be3a060fa6
shalu25kumar <shalu25.kumar@gmail.com>
2025-04-19 02:30:24 -0400

    upload fix

--- my-angular-app/src/app/upload/upload.component.js
+++
@@ -1,5 +0,0 @@
-function updateProgress(percent) {
-    document.querySelector('.progress-fill').style.width = `${percent}%`;
-    document.querySelector('.progress-label').textContent = `${percent}%`;
-  }
-  
