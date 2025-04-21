476f414f6be52ab11a645f7b0059df99f2795cdd
shalu25kumar <shalu25.kumar@gmail.com>
2025-04-21 12:44:30 -0400

    css and html

---
+++ my-angular-app/src/app/ad-search/ad-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-search',
+  imports: [],
+  templateUrl: './ad-search.component.html',
+  styleUrl: './ad-search.component.less'
+})
+export class AdSearchComponent {
+
+}
